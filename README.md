7fc8b0bac5ca55ec67dfd6ec6e7eecad7032fea8
Jan Potoms <potoms.jan@gmail.com>
2014-01-19 21:43:17 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,42 @@
-mongodb-instance
-================
+# mongodb-instance
 
 Downloads and installs mongodb relative to the package, intended for testing purposes.
+
+## installation
+
+   npm install mongodb-instance
+
+## Example
+
+```js
+'use strict';
+
+var mongod = require('mongodb-instance'),
+    path   = require('path'),
+    MongoClient = require('mongodb').MongoClient;
+
+var port = 27000;
+var instance = mongod.start({
+  port: port,
+  dbpath: path.join(__dirname, 'db')
+});
+
+setTimeout(function () {
+  MongoClient.connect('mongodb://localhost:' + port, function (err, db) {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log('Connected to db: ' + db.databaseName);
+      db.close();
+    }
+    
+    setTimeout(instance.stop.bind(instance), 1000);
+  });
+}, 1000);
+```
+
+## Known issues
+
+- Only tested on a mac
+- Suffers from racing conditions when starting up (use setTimeout for now)
+- Do not install globally (`-g`)
---
+++ install.js
@@ -0,0 +1,76 @@
+'use strict';
+
+var util = require('util'),
+    path = require('path'),
+    http = require('http'),
+    tar  = require('tar'),
+    zlib = require('zlib');
+
+
+
+var platformPart = null,
+    extension    = 'tgz',
+    versionPart  = '2.4.9';
+
+if (process.platform === 'linux' && process.arch === 'x64') {
+  platformPart = 'linux-x86_64';
+} else if (process.platform === 'linux') {
+  platformPart = 'linux-i686';
+} else if (
+  process.platform === 'darwin' ||
+  process.platform === 'openbsd' ||
+  process.platform === 'freebsd'
+) {
+  platformPart = 'osx-x86_64';
+} else if (process.platform === 'win32') {
+  console.log('Windows not supported');
+  process.exit(1);
+} else {
+  console.log(
+    'Unexpected platform or architecture: %s - %s',
+    process.platform,
+    process.arch
+  );
+  process.exit(1);
+}
+
+
+
+var downloadUrl = util.format(
+  'http://downloads.mongodb.org/osx/mongodb-%s-%s.%s',
+  platformPart,
+  versionPart,
+  extension
+);
+
+var downloadFolder = path.join(__dirname, 'bin/mongo');
+
+
+function extract(stream) {
+  stream
+    .pipe(zlib.Unzip())
+    .on('error', function (er) {
+      console.error('zlib error: ' + er.message);
+    })
+    .pipe(tar.Extract({
+      path: downloadFolder,
+      strip: 1
+    }))
+    .on('error', function (er) {
+      console.error('tar error: ' + er.message);
+    })
+    .on('entry', function (entry) {
+      console.error('extracting: ' + entry.path);
+    })
+    .on('end', function () {
+      console.error('done');
+    });
+}
+
+function download() {
+  http.get(downloadUrl, extract).on('error', function (e) {
+    console.log('Got error: ' + e.message);
+  });
+}
+
+download();
